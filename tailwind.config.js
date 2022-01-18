8e0c32baca2585f8d2aab67b12a1a414511cd9ca
Aldo Migge <cleepe5@hotmail.com>
2022-01-17 23:58:20 -0300

    Import googleFonts pro tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jetbrains: "JetBrains Mono",
+      },
+    },
   },
   plugins: [],
 };
